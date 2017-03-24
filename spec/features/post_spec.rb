ad5bf839179779a3404be048f188df57e3adac26
Muhcoj <jochum.de.bueger@gmail.com>
2017-03-24 20:50:04 +0100

    Implemented user connection to posts with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe 'navigate' do
 	describe 'index' do
+
+		before do
+			user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Joop", last_name: "Snow")
+			login_as(user, :scope => :user)
+		end 
+
 		it 'can be reached successfully' do
 			visit posts_path
 			expect(page.status_code).to eq(200)
@@ -15,6 +21,8 @@ describe 'navigate' do
 
 	describe 'creation' do
 		before do
+			user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Joop", last_name: "Snow")
+			login_as(user, :scope => :user)
 			visit new_post_path
 		end 
 
@@ -29,6 +37,15 @@ describe 'navigate' do
   		click_on "Save"
 
   		expect(page).to have_content("Some rationale")
+		end
+
+		it 'will have a user associated with it' do
+			fill_in 'post[date]', with: Date.today
+  		fill_in 'post[rationale]', with: "User association"
+  			
+  		click_on "Save"
+
+  		expect(User.last.posts.last.rationale).to eq("User association")
 		end
 	end
 end
